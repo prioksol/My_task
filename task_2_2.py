2fc2cfea151aa55a404b15b64baf5862620b1f01
prioksol <p_oksana@iph.krasn.ru>
2024-09-23 16:43:42 +0700

    Add files via upload

---
+++ task_2_2.py
@@ -0,0 +1,10 @@
+First = input()
+Second = input()
+Third = input()
+if First == Second == Third:
+    print ('3')
+elif First ==  Second or First == Third:
+    print('2')
+elif First != Second and Third:
+    print('0')
+
