ff03f95165e5372b5341f0e8a7763ae098d5a841
prioksol <p_oksana@iph.krasn.ru>
2024-10-04 14:30:00 +0700

    Add files via upload

--- task_3_3.py
+++ task_3_3.py
@@ -1,14 +1,25 @@
-def send_email(message, recipient, *,sender = "university.help@gmail.com"):
-    if ("@" and (".com" or ".ru" or ".net") not in (recipient or sender)) or ("@" or (".com" or ".ru" or ".net")) not in (recipient or sender):
-        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
-    elif recipient == sender:
-        print("Нельзя отправить письмо самому себе!")
-    elif sender == "university.help@gmail.com":
-        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
-    elif sender != "university.help@gmail.com":
-        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+# 1. Функция с параметрами по умолчанию:
 
-send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
-send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
-send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
-send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+def print_params(a = 1, b = 'строка', c = True):
+    print(a, b, c)
+
+print_params()
+print_params(2)
+print_params(0, [4,6,4,6])
+# print_params(54, 11, 16, 8) # Error
+print_params(b = 25)
+print_params(c = [1,2,3])
+
+# 2. Распаковка параметров:
+value_list = [2, 'book', True]
+value_dict = {'a': 3, 'b': 'pen', 'c': True}
+
+print_params(*value_list)
+print_params(**value_dict)
+
+# 3. Распаковка + отдельные параметры:
+
+value_list_2 = (54.32, "Строка")
+print_params(*value_list_2, 42)
+
+
