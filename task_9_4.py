facfbeb24e7753fac774d084439bdd801126c7da
prioksol <p_oksana@iph.krasn.ru>
2024-11-26 14:59:54 +0700

    Add files via upload

---
+++ task_9_4.py
@@ -0,0 +1,37 @@
+# lambda - функция
+
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+
+print(list(map(lambda x, y: x == y, first, second)))
+
+# Замыкание
+
+import random
+
+def get_advanced_writer(file_name):
+
+    def write_everything(*data_set):
+        with open(file_name, 'a', encoding='utf-8') as f:
+            for data in data_set:
+                f.write(str(data) + '\n')
+
+    return write_everything
+
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+# Метод call
+
+class MysticBall:
+
+    def __init__(self, *words):
+        self.words = words
+
+    def __call__(self):
+        return random.choice(self.words)
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
