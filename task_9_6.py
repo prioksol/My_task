0316984cbddd7cca050f32a34f251c340160015c
prioksol <p_oksana@iph.krasn.ru>
2024-11-28 15:49:58 +0700

    Add files via upload

--- task_9_6.py
+++ task_9_6.py
@@ -1,9 +1,16 @@
+from itertools import combinations
+
 def all_variants(text):
-    for i in range(len(text)):
-        for j in range (i + 1, len(text)+1):
-            yield text[i:j]
+    lis = []
+    for i in range(1, len(text)+1):
+        lis = []
+        b = combinations(text, i)
+        lis.extend(b)
+        for j in lis:
+            j = list(j)
+            yield (''.join(j))
 
 
-a = all_variants('abc')
+a = all_variants("abc")
 for i in a:
     print(i)
